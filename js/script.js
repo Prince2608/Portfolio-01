2cc05c54e486777eb9f0be43e2cc6e55509385bb
Prince <66307803+Prince2608@users.noreply.github.com>
2021-11-06 16:44:24 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","Full-stack Developer","Frontend Developer ","Java Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
