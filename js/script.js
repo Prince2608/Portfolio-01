18f763593e5d84fd2fc1f23f689816d540d2c32f
Prince <66307803+Prince2608@users.noreply.github.com>
2026-04-26 17:02:00 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ============================== typing animation ============================ */
 var typed = new Typed(".typing",{
-    strings:["","Full-stack Developer","Frontend Developer ","Java Developer"],
+    strings:["","Project Engineer","SQL Developer ","Data Analyst"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
